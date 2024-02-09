06a61990456d58773692605d383483e04b8d92ed
Lohitht909 <lohithtallapudi909@gmail.com>
2024-02-10 04:47:06 +0530

    New

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import ResState from "./component/Hooks/ResState";
+// import Resturant from "./component/Basics/Resturant";
+// import ResEffect from "./component/Hooks/ResEffect";
+import ResReducer from "./component/Hooks/ResReducer";
+
+const App = () => {
+  return (
+    <div>
+      <ResReducer />
+    </div>
+  );
+};
+
+export default App;
