04d06339894f7dddce7e3a97288d5ecf196425c8
Lohitht909 <lohithtallapudi909@gmail.com>
2024-02-15 17:54:27 +0530

    todolist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Todo from "./component/todolist/todo";
 // import ResState from "./component/Hooks/ResState";
 // import Resturant from "./component/Basics/Resturant";
 // import ResEffect from "./component/Hooks/ResEffect";
-import ResReducer from "./component/Hooks/ResReducer";
+// import ResReducer from "./component/Hooks/ResReducer";
 
 const App = () => {
   return (
     <div>
-      <ResReducer />
+      <Todo />
     </div>
   );
 };
